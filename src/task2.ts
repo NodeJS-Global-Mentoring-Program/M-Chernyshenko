0e1cd461c90bfb2465310dd94eebc9a4944e2f57
Чернышенко Михаил Дмитриевич <Mikhail_Chernyshenko@epam.com>
2020-07-25 14:01:46 +0300

    feat [task 2]: add converting csv to json

---
+++ src/task2.ts
@@ -0,0 +1,30 @@
+const csvtojson = require('csvtojson');
+const fs = require('fs');
+const path = require('path');
+const { pipeline } = require('stream');
+
+const dirPath = path.resolve('csv');
+const csvFileName = 'nodejs-hw1-ex1.csv';
+const jsonFileName = 'nodejs-hw1-ex1.txt';
+
+const readStream = fs.createReadStream(path.join(dirPath, csvFileName));
+const writeStream = fs.createWriteStream(path.join(dirPath, jsonFileName));
+
+const onError = (error: NodeJS.ErrnoException | null): void => {
+  if (error) {
+    return console.error(`Something went wrong: ${error.message}`);
+  }
+  console.log('Convertation success');
+};
+pipeline([readStream, csvtojson(), writeStream], onError);
+
+// In pipe by hand way we need add error handler after every step
+// readStream
+//   .on('error', onError)
+//   .pipe(csvtojson())
+//   .on('error', onError)
+//   .pipe(writeStream)
+//   .on('error', onError)
+//   .on('finish', () => {
+//     return console.log('Convertation success');
+//   });

82c8a97fd236fd5df29359cd6c5d443e0e6726b2
Чернышенко Михаил Дмитриевич <Mikhail_Chernyshenko@epam.com>
2020-07-25 11:38:16 +0300

    feat[task 1.1]: reverse string from STDIN

---
+++ src/taks1.1.ts
@@ -0,0 +1,18 @@
+const { EOL } = require('os');
+const stdin = process.stdin;
+const stdout = process.stdout;
+
+stdout.write(
+  'Print anything and it will be reversed. Print "exit" for program stop.\n'
+);
+const onMessage = (data: Buffer): void => {
+  const message = data.toString().trim();
+  if (message.length === 0) {
+    return;
+  } else if (message === 'exit') {
+    process.exit();
+  }
+  stdout.write(message.split('').reverse().join(''));
+  stdout.write(EOL.repeat(2));
+};
+stdin.on('data', onMessage);
